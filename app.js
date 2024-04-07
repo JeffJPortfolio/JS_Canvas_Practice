0ef45c7033eba0ddf031217e1dc027da08cc120e
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-07 20:48:26 +0900

    color_update

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option")) ;
+const color = document.getElementById("color") ;
+const lineWidth = document.getElementById("line-width") ;
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d") ;
-
 canvas.width= 800;
 canvas.height= 800;
-
-ctx.lineWidth = 2 ;
-
-const colors = [
-    "#1abc9c",
-    "#2ecc71",
-    "#3498db",
-    "#7f8c8d",
-    "#ecf0f1",
-    "#2c3e50",
-    "#27ae60",
-]
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event){
@@ -35,8 +24,27 @@ function onMouseDown(){
 
 function onMouseUp(){
     isPainting = false ;
+    ctx.beginPath() ; 
+
 }
+
+function onLineWidhtChange(event) {
+    ctx.lineWidth = event.target.value ;
+}
+
+function onColorChange(event){
+    ctx.strokeStyle = ctx.fillStyle = event.target.value;
+    }
+
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
+    }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown) ;
 document.addEventListener("mouseup", onMouseUp) ;
+
+lineWidth.addEventListener("change", onLineWidhtChange) ;
+color.addEventListener("change", onColorChange); 
+colorOptions.forEach(color => color.addEventListener("click", onColorClick))
