15738bd0d833b0c7576ebee550e6f237ae30dcc3
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-07 21:05:03 +0900

    Earase update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const modeBtn =document.getElementById("mode-btn");
 const destroyBtn =document.getElementById("destroy-btn");
+const esraserBtn =document.getElementById("esraser-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option")) ;
 const color = document.getElementById("color") ;
 const lineWidth = document.getElementById("line-width") ;
@@ -62,11 +63,24 @@ function onCanvasclick() {
     }
 }
 
+function onDestroyClick() {
+    ctx.fillStyle = "white" ; 
+    ctx.fillRect(0,0,800,800);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white" ;
+    isFilling = false ;
+    modeBtn.innerText = "Fill" ;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown) ;
 document.addEventListener("mouseup", onMouseUp) ;
 canvas.addEventListener("click", onCanvasclick) ;
 lineWidth.addEventListener("change", onLineWidhtChange) ;
 color.addEventListener("change", onColorChange); 
-colorOptions.forEach(color => color.addEventListener("click", onColorClick))
-modeBtn.addEventListener("click", onModeClick)
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
