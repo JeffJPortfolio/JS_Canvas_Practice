3d85b8c70b5b94a0385bb18a0ce6a94161df331a
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-07 20:58:12 +0900

    fill updated

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+const modeBtn =document.getElementById("mode-btn");
+const destroyBtn =document.getElementById("destroy-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option")) ;
 const color = document.getElementById("color") ;
 const lineWidth = document.getElementById("line-width") ;
@@ -7,6 +10,7 @@ canvas.width= 800;
 canvas.height= 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event){
     if (isPainting) {
@@ -41,10 +45,28 @@ function onColorClick(event){
     ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
     }
 
+function onModeClick(){
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else {
+        isFilling = true ;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasclick() {
+    if(isFilling) {
+        ctx.fillRect(0,0,800,800);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown) ;
 document.addEventListener("mouseup", onMouseUp) ;
-
+canvas.addEventListener("click", onCanvasclick) ;
 lineWidth.addEventListener("change", onLineWidhtChange) ;
 color.addEventListener("change", onColorChange); 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick))
+modeBtn.addEventListener("click", onModeClick)
